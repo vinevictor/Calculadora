de9599e2d0845acab0d93b2cdf451518a2400ca8
Vinicius Victor <vine.victordev@gmail.com>
2022-10-24 21:35:42 -0300

    Função Soma

--- script.js
+++ script.js
@@ -3,3 +3,8 @@
 var num1 = parseInt(prompt("Digite o Primeiro Numero:"));
 var num2 = parseInt(prompt("Digite o Segundo Numero:"));
 
+
+function SomaDosNumeros() {
+    var soma = num1 + num2;
+    return soma;
+}
