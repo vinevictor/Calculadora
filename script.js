70d50bbcc47f288bacfd92ba0bb383ee0b017661
Vinicius Victor <vine.victordev@gmail.com>
2022-10-24 22:00:27 -0300

    Escolha da Operação

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 // Lista de Exercicios - Calculadora
 
-var num1 = parseInt(prompt("Digite o Primeiro Numero:"));
-var num2 = parseInt(prompt("Digite o Segundo Numero:"));
+var repeat = true;
+while (repeat) {
+    var num1 = parseInt(prompt("Digite o Primeiro Numero:"));
+    var num2 = parseInt(prompt("Digite o Segundo Numero:"));
+
+    var escolha = parseInt(prompt("Digite 1 para Soma, 2 Para Subtração, 3 Para Multiplicação, 4 para Divisão."));
+    if (escolha == 1) {
+        console.log("Soma = ", SomaDosNumeros());
+    } else if (escolha == 2) {
+        console.log("Subtração = ", SubtracaoDosNumeros());
+    } else if (escolha == 3) {
+        console.log("Multiplicação = ", MultiplicacaoDosNumeros());
+    } else if (escolha == 4) {
+        console.log("Divisão = ", DivisaoDosNumeros());
+    }
+
+    repeat = (prompt("Deseja fazer outra operação? s/n"));
+
+    if (repeat == "n" || repeat == "N") {
+        repeat = false;
+    }
+};
 
 
 function SomaDosNumeros() {
@@ -14,7 +34,7 @@ function SubtracaoDosNumeros() {
     return sub;
 }
 
-function MutiplicacaoDosNumeros() {
+function MultiplicacaoDosNumeros() {
     var mult = num1 * num2;
     return mult;
 }
